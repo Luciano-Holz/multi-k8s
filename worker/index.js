f3b3ba70764db66bc91449e3c6bb849443836caa
Luciano Holz <luciano.holz@compasso.com.br>
2022-05-05 11:06:02 -0300

    added deployment

--- worker/index.js
+++ worker/index.js
@@ -7,6 +7,7 @@ const redisClient = redis.createClient({
   retry_strategy: () => 1000,
 });
 const sub = redisClient.duplicate();
+console.log(sub);
 
 function fib(index) {
   if (index < 2) return 1;
